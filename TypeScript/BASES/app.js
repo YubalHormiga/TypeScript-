bbf385aa14105ab5305dfac4f21cdfa21501d96d
YubalHormiga <yubalhh@gmail.com>
2023-04-19 18:21:45 +0200

    Seccion 1  introduccion

---
+++ TypeScript/BASES/app.js
@@ -0,0 +1,9 @@
+"use strict";
+const msg = 'Hola¡¡';
+console.log(msg);
+const hero = {
+    name: 'Superman',
+    age: 45
+};
+hero.age = 50;
+console.log(hero.age + 1);
---
+++ TypeScript/BASES/app.ts
@@ -0,0 +1,10 @@
+
+const msg: string = 'Hola¡¡'
+console.log(msg)
+
+const hero = {
+    name: 'Superman',
+    age: 45
+}
+hero.age = 50
+console.log(hero.age + 1)
