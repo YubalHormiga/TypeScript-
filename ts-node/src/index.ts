56646ab75f2237e15239d631f4aeef03b5eef44f
YubalHormiga <yubalhh@gmail.com>
2024-01-12 11:08:32 +0100

    curso finalizado

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,3 +1,5 @@
+//!Tema 11
+
 import { getPokemon } from './generics/get-pokemon';
 
 
@@ -7,6 +9,21 @@ getPokemon(4)
     .then( pokemon => console.log( pokemon.abilities[0].ability.url ) )
     .catch( error => console.error( error ) )
     .finally( () => console.log('Fin de getPokemon')  )
+//!Fin tema 11
 
+//?Tema
+
+// import { Pokemon } from './decorators/pokemon-class';
+
+
+
+// const charmander = new Pokemon('Charmander');
+
+// // (Pokemon.prototype as any).customName = 'Pikachu'
+
+// // console.log(charmander.savePokemonToDB(50));
+// // charmander.savePokemonToDB(10);
+// charmander.publicApi = 'https://fernando-herrera.com';
+// console.log( charmander )
 
 
